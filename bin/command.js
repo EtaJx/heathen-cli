f0cb9d46d3829881a88f75b15b77996e5470091b
Neras <medianeras57@gmail.com>
2019-04-30 11:15:09 +0800

    create template by string

--- bin/command.js
+++ bin/command.js
@@ -6,29 +6,24 @@ const fs = require('fs');
 const allowFiles = ['md'];
 
 program
-.version('0.0.1')
-.option('-n, --new <file>', 'init an new article')
-.action((cmd) => {
-  if(!cmd.new) {
-    throw Error('miss arguments, plz checkout you command');
-  }
-  const fileName = cmd.new.split('.')[0];
-  const fileSuffix = cmd.new.split('.')[1];
-  if(!allowFiles.includes(fileSuffix)) {
-    throw Error('It must be a markdown file');
-  }
-  const title = `title: ${fileName}`
-  const date = `date: ${new Date().toLocaleString().replace(/\//g, '-')}`
-  const templatePath = path.resolve(__dirname, '../template');
-  fs.readFile(`${templatePath}/template.md`, (err, data) => {
-    if(err) {
-      console.log(err);
-      exit(1);
+  .version('0.0.1')
+  .option('-n, --new <file>', 'init an new article')
+  .action((cmd) => {
+    if (!cmd.new) {
+      throw Error('miss arguments, plz checkout you command');
     }
-    let contents = data.toString().split('\n');
-    contents[1] = title;
-    contents[2] = date;
-    fs.writeFileSync(`${process.cwd()}/_post/${fileName}.md`, contents.join('\n'), 'utf8');
-  });
-})
-.parse(process.argv);
+    const fileName = cmd.new.split('.')[0];
+    const fileSuffix = cmd.new.split('.')[1];
+    if (!allowFiles.includes(fileSuffix)) {
+      throw Error('It must be a markdown file');
+    }
+    const title = `title: ${fileName}`
+    const date = `date: ${new Date().toLocaleString().replace(/\//g, '-')}`
+    const template = `\`\`\`yaml\n${title}\n${date}\n\`\`\``;
+    try {
+      fs.writeFileSync(`${process.cwd()}/_post/${fileName}.md`, template, 'utf8');
+    } catch (err) {
+      throw Error(err);
+    }
+  })
+  .parse(process.argv);
--- template/template.md
+++
@@ -1,4 +0,0 @@
-```yaml
-title:
-date: 
-```
